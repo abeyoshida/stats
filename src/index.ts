5e2a15203c5a4e9a4add8bf5c16506d1169d1d11
Abe Yoshida <abeyoshida@gmail.com>
2022-09-20 12:52:48 -0400

    parse data from csv file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('hey foo');
+import { CsvFileReader } from './CsvFileReader';
+
+const reader = new CsvFileReader('./resource/football.csv');
+reader.read();
+
+// enum - enumeration
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games.`);
